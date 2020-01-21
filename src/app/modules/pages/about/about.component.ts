5049079acc6db2513eeaf6877630ac5efae9d7d7
windcloak <xfirecloak@gmail.com>
2020-01-20 21:59:41 -0600

    Added text to about page

--- src/app/modules/pages/about/about.component.ts
+++ src/app/modules/pages/about/about.component.ts
@@ -7,6 +7,67 @@ import { Component } from '@angular/core';
 })
 export class AboutPageComponent {
 
+  profiles = [
+    {
+      title: 'Name',
+      description: 'May'
+    },
+    {
+      title: 'Birthday',
+      description: 'Jan 14'
+    },
+    {
+      title: 'Height',
+      description: '164 cm'
+    },
+    {
+      title: 'Blood Type',
+      description: 'O+'
+    },
+    {
+      title: 'Ethnicity',
+      description: 'Chinese-American'
+    },
+    {
+      title: 'Cosplaying Since',
+      description: '2010'
+    },
+    {
+      title: 'Hobbies',
+      description: 'Cosplay, photography, anime, SSBU, Pokemon, Webtoons'
+    },
+    {
+      title: 'Occupation',
+      description: 'Software Developer'
+    },
+  ];
+
+  websiteInfo = [
+    {
+      title: 'Angular 8',
+      link: 'https://angular.io'
+    },
+    {
+      title: 'Bootstrap',
+      link: 'https://getbootstrap.com/'
+    },
+    {
+      title: 'Font Awesome',
+      link: 'https://fontawesome.com/'
+    },
+    {
+      title: 'Ngx-gallery',
+      link: 'https://github.com/lukasz-galka/ngx-gallery'
+    },
+    {
+      title: 'Ngx-scrolltop',
+      link: 'https://github.com/bartholomej/ngx-scrolltop'
+    },
+    {
+      title: 'Ng-lazyload-image',
+      link: 'https://github.com/tjoskar/ng-lazyload-image'
+    },
+  ];
 
   constructor() { }
 
