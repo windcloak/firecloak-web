b7b90cf7d19e0d824011b5cb93f3ac9848bab0e2
May Guo <40670609+windcloak@users.noreply.github.com>
2021-02-04 23:33:39 -0600

    Removed deprecated use of Font Awesome library

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,3 +1,2 @@
 export * from './components';
 export * from './module';
-export * from './icons';
--- src/app/shared/module.ts
+++ src/app/shared/module.ts
@@ -5,13 +5,18 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { COMPONENTS } from './components';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// Font Awesome Icons
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faFacebookSquare,
+  faInstagram,
+  faTwitter,
+  faDeviantart } from '@fortawesome/free-brands-svg-icons';
+
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { DisqusModule } from 'ngx-disqus';
 import { DISQUS_SHORTNAME } from 'ngx-disqus';
-import './icons';
 import 'hammerjs';
 
 import { HammerModule } from '@angular/platform-browser'; // image swipe
@@ -69,4 +74,12 @@ export class MyHammerConfig extends HammerGestureConfig {
   ],
 })
 export class SharedModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(
+      faFacebookSquare,
+      faInstagram,
+      faTwitter,
+      faDeviantart,
+    );
+  }
 }
